bb99df21da3b03b4c4e16ec6a4b369e2b8803352
Theodore Chuang <chuangtheodore@gmail.com>
2019-12-03 23:51:22 +1100

    [game-stats] Refactor: inheritance to composition (delegation)

--- game-stats/src/index.test.ts
+++ game-stats/src/index.test.ts
@@ -1,21 +1,25 @@
-import { manUnitedWins } from './index'
 import { MatchReader } from './MatchReader'
+import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
+import { manUnitedWins } from './index'
 
-const reader = new MatchReader('football.csv')
-reader.read()
+const csvFileReader = new CsvFileReader('football.csv')
+
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 test('MatchReader can read the CSV file', () => {
-  expect(reader.data).toBeTruthy()
+  expect(matchReader.matches).toBeTruthy()
 })
 
 test('MatchReader can parse the data into a 2D array', () => {
-  expect(Array.isArray(reader.data)).toBeTruthy()
-  expect(Array.isArray(reader.data[0])).toBeTruthy()
+  expect(Array.isArray(matchReader.matches)).toBeTruthy()
+  expect(Array.isArray(matchReader.matches[0])).toBeTruthy()
 })
 
 test('MatchReader transforms first row as expected', () => {
   const row0 = [new Date(2018, 7, 10), 'Man United', 'Leicester', 2, 1, 'H', 'A Marriner']
-  expect(reader.data[0]).toStrictEqual(row0)
+  expect(matchReader.matches[0]).toStrictEqual(row0)
 })
 
 test('Calculated Man United wins to be 18', () => {
--- game-stats/src/index.ts
+++ game-stats/src/index.ts
@@ -1,12 +1,16 @@
 import { MatchReader } from './MatchReader'
+import { CsvFileReader } from './CsvFileReader'
 import { MatchResult } from './MatchResult'
 
-const reader = new MatchReader('football.csv')
-reader.read()
+// "Favor object composition over class inheritance"
+const csvFileReader = new CsvFileReader('football.csv')
+
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 export const manUnitedWins = (): number => {
   let manUnitedWins = 0
-  for (let match of reader.data) {
+  for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
       manUnitedWins++
     }
