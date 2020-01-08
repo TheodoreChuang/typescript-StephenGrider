522af85f6eecaf18b13d14568a617706c8116a93
Theodore Chuang <chuangtheodore@gmail.com>
2020-01-08 22:37:07 +1100

    [web-framework] Add Collection class for User

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,22 +1,31 @@
-import { User } from './models/User'
+// import { User } from './models/User'
+import { Collection } from './models/Collection'
 
 export const rootUrl = 'http://localhost:3000'
 
-const jc = new User({ name: 'John Citizen', age: 99, id: 84739532 })
+// const jc = User.buildUser({ name: 'John Citizen', age: 99, id: 1234 })
 
-jc.on('change', () => console.warn('User Attributes Changed:\n', jc))
-jc.on('save', () => console.warn('User Saved:\n', jc))
-jc.on('error', () => console.error('Error saving user'))
+// jc.on('change', () => console.warn('User Attributes Changed:\n', jc))
+// jc.on('save', () => console.warn('User Saved:\n', jc))
+// jc.on('error', () => console.error('Error saving user'))
 
-console.log(jc.get('name'))
+// console.log(jc.get('name'))
 
-jc.set({ name: 'John Civilian', age: 1 })
-console.log(jc.get('name'))
+// jc.set({ name: 'John Civilian', age: 1 })
+// console.log(jc.get('name'))
 
-jc.on('updated', () => console.log('update 1'))
-jc.on('updated', () => console.log('update 2'))
-jc.trigger('updated')
+// jc.on('updated', () => console.log('update 1'))
+// jc.on('updated', () => console.log('update 2'))
+// jc.trigger('updated')
 
-jc.fetch()
+// jc.fetch()
 
-jc.save()
+// jc.save()
+
+const collection = new Collection(`${rootUrl}/users`)
+
+collection.on('change', () => {
+  console.log(collection)
+})
+
+collection.fetch()
